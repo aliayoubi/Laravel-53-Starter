029e4a9f1d7ac74627e3d28785af30d0c85ff251
Sarfraz <sarfraznawaz2005@gmail.com>
2016-09-19 23:26:15 +0500

    updated

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container">
         <a id="logo-container" href="{{ url('/') }}" class="brand-logo">
-            {{ config('app.name', 'Laravel') }}
+            {{ config('app.name', 'App Name') }}
         </a>
 
         <ul class="right hide-on-med-and-down">
