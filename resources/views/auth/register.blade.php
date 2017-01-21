8efc8ea96d06c404c736c2523663bcbae2d2a3eb
Sarfraz <sarfraznawaz2005@gmail.com>
2017-01-21 22:44:04 +0500

    updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-primary">
+                <div class="panel panel-{{$errors->any() ? 'danger' : 'primary'}}">
                     <div class="panel-heading"><i class="glyphicon glyphicon-user"></i> Account Details</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
